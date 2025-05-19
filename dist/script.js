f7e4721c015eba8c701282985edf43265d665462
Ish-Avanzado <isiaavanzado@gmail.com>
2025-05-19 09:32:49 -0400

    updating files

--- dist/script.js
+++ dist/script.js
@@ -2,7 +2,9 @@ const burger = document.querySelector(".burger__menu");
 const hNav1 = document.querySelector(".header__nav1");
 const hNav2 = document.querySelector(".header__nav2");
 burger.addEventListener("click",  ()  => {
-    burger.classList.toggle(".open");
-    hNav1.classList.toggle(".open");
-    hNav2.classList.toggle(".open");
-})
+    burger.classList.toggle("open");
+    hNav1.classList.toggle("open");
+    hNav2.classList.toggle("open");
+});
+
+
